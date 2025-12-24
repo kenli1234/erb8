4913f85e6b84ff9e29f233a89e8b01eb2e18a6fb
kenli1234 <ken_li1234@yahoo.com.hk>
2025-12-24 14:36:18 +0800

    Step 5 : listings and doctors model

--- config/settings.py
+++ config/settings.py
@@ -85,8 +85,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'clinic',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
     }
 }
 
