5474ff42169807e50712fb820350fa75221746bb
kenli1234 <ken_li1234@yahoo.com.hk>
2025-12-30 11:23:53 +0800

    Step 7 : listings pagination

--- listings/views.py
+++ listings/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Listing
+from django.core.paginator import Paginator
 
 # Create your views here.
 def listings(request):
 #    return HttpResponse("<h1>Hello World</h1>")
-    print(request, request.path)
+    #print(request, request.path)
+    #print('aaaaaaaaaa')
+    #print('listings', len(listings))
 
-    print('?????????????')
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.filter(is_published=True)
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    context = {"listings": paged_listings}
+    
+    return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
     print(request, request.path)
