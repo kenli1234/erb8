54ca226aa2ed193de1704da4d108c2c740573fb7
kenli1234 <ken_li1234@yahoo.com.hk>
2025-12-31 17:49:28 +0800

    Step 10 : search

--- listings/views.py
+++ listings/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from listings.models import Listing
 from django.core.paginator import Paginator
+from listings.choices import district_choices, room_choices, rooms_choices
+from django.db.models import Q
 
 # Create your views here.
 def listings(request):
@@ -26,6 +28,40 @@ def listing(request, listing_id):
 #    return HttpResponse("<h1>about</h1>")
 
 def search(request):
-    print(request, request.path)
-    return render(request, 'listings/search.html')
+    #print(request, request.path)
+    queryset_list = Listing.objects.order_by("-list_date")
+    if 'keywords' in request.GET:
+        keywords = request.GET["keywords"]
+        if keywords:
+            queryset_list = queryset_list.filter(Q(description__icontains=keywords) | Q(title__icontains=keywords) | Q(doctor__name__icontains=keywords))
+            print('keywords --', queryset_list)
+    if 'district' in request.GET:
+        district = request.GET["district"]
+        if district:
+            queryset_list = queryset_list.filter(district__iexact=district)
+            print('district --', queryset_list)
+    if 'rooms' in request.GET:
+        rooms = request.GET["rooms"]
+        if rooms:
+            queryset_list = queryset_list.filter(rooms__gte=rooms)
+    if 'room_type' in request.GET:
+        room_type = request.GET["room_type"]
+        if room_type:
+            queryset_list = queryset_list.filter(room_type__iexact=room_type)
+
+    print('*******district**********', request.GET.get('district'))
+    print('*******queryset_list**********', len(queryset_list))
+
+    paginator = Paginator(queryset_list, 3)
+    page = request.GET.get('page', 1)
+    paged_listings = paginator.get_page(page)
+
+    print('*******paged_listings.has_other_pages**********', paged_listings.has_other_pages)
+    
+    context = {"listings": paged_listings,
+               "district_choices": district_choices, 
+               "room_choices": room_choices, 
+               "rooms_choices": rooms_choices,
+               "values": request.GET}
+    return render(request, 'listings/search.html', context)
 #    return HttpResponse("<h1>about</h1>")
--- pages/views.py
+++ pages/views.py
@@ -2,27 +2,41 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from listings.models import Listing
 from doctors.models import Doctor
+from listings.choices import district_choices, room_choices, rooms_choices
 
-# Create your views here.
+# ! Create your views here.
+# TODO: update index and about views
+# # Homepage
+# ? List 3 latest listings
+# * dafwrgtrwdasfgeg
 def index(request):
 #    return HttpResponse("<h1>Hello World</h1>")
 #    print(request, request.path)
 #    return render(request, 'pages/index.html')
 #    return render(request, 'pages/index.html',{'anything':'something', 'number':1234})
 
-    print('TEST*************AAAAA')
+    #print('TEST*************AAAAA')
     #listings = Listing.objects.all()
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
-    context = {"listings": listings}
-    print('TEST*************', listings)
-    return render(request, 'pages/index.html', {"listings": listings})
+    #context = {"listings": listings}
+    #print('TEST*************', listings)
+    context = {"listings": listings,
+               "district_choices": district_choices, 
+               "room_choices": room_choices, 
+               "rooms_choices": rooms_choices}
+    return render(request, 'pages/index.html', context)
 
 def about(request):
-    print('TEST*************!!!')
+    #print('TEST*************!!!')
     doctors = Doctor.objects.order_by("-hire_date")[:3]
     mvp_doctors = Doctor.objects.all().filter(is_mvp=True)
-    context = {"doctors": doctors, "mvp_doctors": mvp_doctors}
-    print('mvp_doctors: --', len(mvp_doctors))
-    print(request, request.path)
+    context = {"doctors": doctors,
+               "mvp_doctors": mvp_doctors}
+
+
+    #print('***************mvp_doctors:********:', len(mvp_doctors))
+
+
+    #print(request, request.path)
     return render(request, 'pages/about.html', context)
 #    return HttpResponse("<h1>about</h1>")
